28084e165ab6c8948a498f1f96266f4a6dbfaf10
michaelharvey22 <MichaelHarvey@Michaels-MacBook-Air.local>
2016-10-16 19:56:50 +1100

    google map styling, initial centre on user work
    google map styling trying to mimic MQ map,
    initial work on trying to implement a button

--- Hop/www/js/map.js
+++ Hop/www/js/map.js
@@ -17,7 +17,87 @@
 		    	zoom: 16,
 		    	streetViewControl: false,
 		    	disableDefaultUI: true,
-		    	zoomControl: true
+		    	zoomControl: true,
+          styles: [
+            // {elementType: 'geometry', stylers: [{color: '#E2DECB'}]}, //E2DECB background, A9A086 buildings
+            // {elementType: 'labels.text.stroke', stylers: [{color: '#000000'}]},
+            // {elementType: 'labels.text.fill', stylers: [{color: '#ffffff'}]},
+            // {
+            //   featureType: 'administrative.locality',
+            //   elementType: 'labels.text.fill',
+            //   stylers: [{color: '#d59563'}]
+            // },
+            // {
+            //   featureType: 'poi',
+            //   elementType: 'labels.text.fill',
+            //   stylers: [{color: '#d59563'}]
+            // },
+            // {
+            //   featureType: 'poi.park',
+            //   elementType: 'geometry',
+            //   stylers: [{color: '#263c3f'}]
+            // },
+            // {
+            //   featureType: 'poi.park',
+            //   elementType: 'labels.text.fill',
+            //   stylers: [{color: '#6b9a76'}]
+            // },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#ffffff'}]
+            },
+            // {
+            //   featureType: 'road',
+            //   elementType: 'geometry.stroke',
+            //   stylers: [{color: '#212a37'}]
+            // },
+            // {
+            //   featureType: 'road',
+            //   elementType: 'labels.text.fill',
+            //   stylers: [{color: '#9ca5b3'}]
+            // },
+            // {
+            //   featureType: 'road.highway',
+            //   elementType: 'geometry',
+            //   stylers: [{color: '#746855'}]
+            // },
+            // {
+            //   featureType: 'road.highway',
+            //   elementType: 'geometry.stroke',
+            //   stylers: [{color: '#1f2835'}]
+            // },
+            // {
+            //   featureType: 'road.highway',
+            //   elementType: 'labels.text.fill',
+            //   stylers: [{color: '#f3d19c'}]
+            // },
+            // {
+            //   featureType: 'transit',
+            //   elementType: 'geometry',
+            //   stylers: [{color: '#2f3948'}]
+            // },
+            // {
+            //   featureType: 'transit.station',
+            //   elementType: 'labels.text.fill',
+            //   stylers: [{color: '#d59563'}]
+            // },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#72ADD5'}]
+            }
+            // {
+            //   featureType: 'water',
+            //   elementType: 'labels.text.fill',
+            //   stylers: [{color: '#515c6d'}]
+            // },
+            // {
+            //   featureType: 'water',
+            //   elementType: 'labels.text.stroke',
+            //   stylers: [{color: '#17263c'}]
+            // }
+          ]
 		    };
     map = new google.maps.Map(document.getElementById("map"), opts);
 
@@ -131,4 +211,13 @@
       google.maps.event.addListener(draggablemarker, 'click', function (event) {
         console.log('{"lat": ' + this.getPosition().lat() + ', "lng": ' + this.getPosition().lng() +'},');
       });
+    }
+
+    function currentlocation(position) {
+      var campusBorder = new google.maps.Polygon({paths: campusCoords});
+      var currentlocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      if(google.maps.geometry.poly.containsLocation(currentlocation, campusBorder)){
+        google.maps.panTo(position.coords.latitude, position.coords.longitude);
+      }
+
     }
